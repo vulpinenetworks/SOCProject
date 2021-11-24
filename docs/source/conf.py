dec144bc13f05a8c563e34b49ce9bdc90ae62410
vulpinenetworks <94942720+vulpinenetworks@users.noreply.github.com>
2021-11-23 21:54:17 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,113 +6,45 @@ project = 'SOC IDS Project'
 copyright = '2021, Connor Genyk and Kaushalbhai Patel '
 author = 'Connor Genyk and Kaushalbhai Patel'
 
-# -*- coding: utf-8 -*-
+release = '0.1'
+version = '0.1.0'
 
-import sys
-import os
-import re
-
-# Prefer to use the version of the theme in this repo
-# and not the installed version of the theme.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('./demo/'))
-
-import sphinx_rtd_theme
-from sphinx_rtd_theme import __version__ as theme_version
-from sphinx_rtd_theme import __version_full__ as theme_version_full
-from sphinx.locale import _
-
-project = u'Read the Docs Sphinx Theme'
-slug = re.sub(r'\W+', '-', project.lower())
-version = theme_version
-release = theme_version_full
-author = u'Dave Snider, Read the Docs, Inc. & contributors'
-copyright = author
-language = 'en'
+# -- General configuration
 
 extensions = [
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
-    'sphinx_rtd_theme',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-templates_path = ['_templates']
-source_suffix = '.rst'
-exclude_patterns = []
-locale_dirs = ['locale/']
-gettext_compact = False
-
-master_doc = 'index'
-suppress_warnings = ['image.nonlocal_uri']
-pygments_style = 'default'
-
 intersphinx_mapping = {
-    'rtd': ('https://docs.readthedocs.io/en/stable/', None),
+    'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'logo_only': True,
-    'navigation_depth': 5,
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
-html_context = {}
 
-if not 'READTHEDOCS' in os.environ:
-    html_static_path = ['_static/']
-    html_js_files = ['debug.js']
-
-    # Add fake versions for local QA of the menu
-    html_context['test_versions'] = list(map(
-        lambda x: str(x / 10),
-        range(1, 100)
-    ))
-
-html_logo = "demo/static/logo-wordmark-light.svg"
-html_show_sourcelink = True
-
-htmlhelp_basename = slug
-
-
-latex_documents = [
-  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
-]
-
-man_pages = [
-    ('index', slug, project, [author], 1)
-]
-
-texinfo_documents = [
-  ('index', slug, project, author, slug, project, 'Miscellaneous'),
-]
-
-
-# Extensions to theme docs
-def setup(app):
-    from sphinx.domains.python import PyField
-    from sphinx.util.docfields import Field
-
-    app.add_object_type(
-        'confval',
-        'confval',
-        objname='configuration value',
-        indextemplate='pair: %s; configuration value',
-        doc_field_types=[
-            PyField(
-                'type',
-                label=_('Type'),
-                has_arg=False,
-                names=('type',),
-                bodyrolename='class'
-            ),
-            Field(
-                'default',
-                label=_('Default'),
-                has_arg=False,
-                names=('default',),
-            ),
-        ]
-    )
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
